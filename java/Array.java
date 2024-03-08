44623c712e7b618abbd377d8d24d059dbfc3bcd4
vikas-saini-7 <vikas289888@gmail.com>
2024-03-08 14:03:43 +0530

    array

---
+++ java/Array.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        // int[] arr = {1, 2,3, 4, 5};
+        int[] arr  = new int[5];
+
+        Scanner in = new Scanner(System.in);
+
+        for (int i = 0; i < arr.length; i ++){
+            arr[i] = in.nextInt();
+        }
+        
+
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+    
+}
